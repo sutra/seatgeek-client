99430943f3a0e1edc5a83708dbac959d4dc306aa
Joe Zhou <zhoushuqun@gmail.com>
2023-05-18 23:50:32 +0800

    Add interfaces for cached client.

--- seatgeek-client-cached/src/main/java/org/oxerr/seatgeek/client/cached/CachedSeatGeekClient.java
+++ seatgeek-client-cached/src/main/java/org/oxerr/seatgeek/client/cached/CachedSeatGeekClient.java
@@ -1,33 +1,7 @@
 package org.oxerr.seatgeek.client.cached;
 
-import org.oxerr.seatgeek.client.SeatGeekClient;
-import org.redisson.api.RedissonClient;
+public interface CachedSeatGeekClient {
 
-public class CachedSeatGeekClient {
-
-	private final SeatGeekClient seatGeekClient;
-
-	private final CachedListingService cachedListingService;
-
-	public CachedSeatGeekClient(
-		SeatGeekClient seatGeekClient,
-		RedissonClient redissonClient,
-		String keyPrefix
-	) {
-		this.seatGeekClient = seatGeekClient;
-		this.cachedListingService = new CachedListingService(
-			seatGeekClient.getListingService(),
-			redissonClient,
-			keyPrefix
-		);
-	}
-
-	public SeatGeekClient getClient() {
-		return this.seatGeekClient;
-	}
-
-	public CachedListingService getCachedListingService() {
-		return this.cachedListingService;
-	}
+	CachedListingService getCachedListingService();
 
 }
--- seatgeek-client-cached/src/main/java/org/oxerr/seatgeek/client/cached/model/Status.java
+++ seatgeek-client-cached/src/main/java/org/oxerr/seatgeek/client/cached/model/Status.java
@@ -1,5 +1,8 @@
 package org.oxerr.seatgeek.client.cached.model;
 
+/**
+ * Status of the {@link CachedListing}.
+ */
 public enum Status {
 
 	PENDING_LIST,
