5a5c03effddf7c5ba0df18ceaa01c0f2124138d8
Joe Zhou <zhoushuqun@gmail.com>
2023-05-19 01:38:18 +0800

    Add doc.

--- seatgeek-client-cached/src/main/java/org/oxerr/seatgeek/client/cached/CachedListingService.java
+++ seatgeek-client-cached/src/main/java/org/oxerr/seatgeek/client/cached/CachedListingService.java
@@ -1,13 +1,23 @@
 package org.oxerr.seatgeek.client.cached;
 
 import org.oxerr.seatgeek.client.cached.model.Event;
+import org.oxerr.seatgeek.client.cached.model.Status;
 
 public interface CachedListingService {
 
 	void updateEvent(Event event);
 
+	/**
+	 * Returns the size of the cache.
+	 *
+	 * @return the size of the cache.
+	 */
 	long cacheSize();
 
+	/**
+	 * Returns the listing count which status is {@link Status#LISTED}
+	 * @return
+	 */
 	long listedCount();
 
 }
